972bf3a9801d7b3dd8232a870dfb9590040bb643
sjistheboss <sjistheboss@gmail.com>
2018-09-09 14:35:24 +0600

    Add files via upload

---
+++ small_bot_with_IR_simple_/small_bot_with_IR_simple_.ino
@@ -0,0 +1,64 @@
+int lm1 = 2;
+int lm2 = 3;
+int rm1 = 4;
+int rm2 = 5;
+int sensorPin = A0;
+int sensorData;
+boolean freeze=false;
+int mainThreshold=600;
+
+void setup() {
+
+  pinMode(lm1, OUTPUT);
+  pinMode(lm2, OUTPUT);
+  pinMode(rm1, OUTPUT);
+  pinMode(rm2, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  while (Serial.available() == 0 ) {
+  }
+  
+  char val = Serial.read() ;
+
+ 
+ if(freeze==false){
+    sensorData = analogRead(sensorPin);
+    if (sensorData >= mainThreshold) {
+    freeze = true;
+  }
+
+    if (val == 'F') {
+      
+      digitalWrite(lm1, HIGH);      digitalWrite(rm1, HIGH);
+      digitalWrite(lm2, LOW);      digitalWrite(rm2, LOW);
+    }
+
+    else if (val == 'B')
+    {
+      digitalWrite(lm2, HIGH);   digitalWrite(rm2, HIGH);
+      digitalWrite(lm1, LOW);  digitalWrite(rm1, LOW);
+
+    }
+    else if (val == 'R')
+    {
+      digitalWrite(lm1, HIGH);  digitalWrite(rm2, HIGH);
+      digitalWrite(lm2, LOW);       digitalWrite(rm1, LOW);
+    }
+   
+    else if (val == 'L')
+    {
+      digitalWrite(lm2, HIGH);  digitalWrite(rm1, HIGH);
+      digitalWrite(lm1, LOW);       digitalWrite(rm2, LOW);
+    }
+
+    else  {
+      digitalWrite(lm1, LOW);      digitalWrite(rm1, LOW);
+      digitalWrite(lm2, LOW);       digitalWrite(rm2, LOW);
+
+    }
+
+ }
+}
