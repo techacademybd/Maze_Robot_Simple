39dd154aa675100400a3b75babb01b3a94c369a1
Muhammad Irfanul Haque <mdicmdih@gmail.com>
2018-10-07 16:49:10 +0600

    Updated variable names and spacing

--- small_bot_with_IR_simple_/small_bot_with_IR_simple_.ino
+++ small_bot_with_IR_simple_/small_bot_with_IR_simple_.ino
@@ -1,7 +1,7 @@
-int lm1 = 2;
-int lm2 = 3;
-int rm1 = 4;
-int rm2 = 5;
+int Lm1 = 2;
+int Lm2 = 3;
+int Rm1 = 4;
+int Rm2 = 5;
 int ENLeft = 9;
 int ENRight = 10;
 
@@ -12,17 +12,17 @@ int mainThreshold = 600;
 
 void setup() {
 
-  pinMode(lm1, OUTPUT);
-  pinMode(lm2, OUTPUT);
-  pinMode(rm1, OUTPUT);
-  pinMode(rm2, OUTPUT);
+  pinMode(Lm1, OUTPUT);
+  pinMode(Lm2, OUTPUT);
+  pinMode(Rm1, OUTPUT);
+  pinMode(Rm2, OUTPUT);
   pinMode(ENLeft, OUTPUT);
   pinMode(ENRight, OUTPUT);
 
   analogWrite(ENLeft, 100);
   analogWrite(ENRight, 100);
 
-  
+
   Serial.begin(9600);
 }
 
@@ -30,45 +30,40 @@ void loop() {
 
   while (Serial.available() == 0 ) {
   }
-  
+
   char val = Serial.read() ;
 
- 
- if(freeze==false){
+
+  if (freeze == false) {
     sensorData = analogRead(sensorPin);
     if (sensorData >= mainThreshold) {
-    freeze = true;
-  }
+      freeze = true;
+    }
 
     if (val == 'F') {
-      
-      digitalWrite(lm1, HIGH);      digitalWrite(rm1, HIGH);
-      digitalWrite(lm2, LOW);      digitalWrite(rm2, LOW);
+      digitalWrite(Lm1, HIGH);      digitalWrite(Rm1, HIGH);
+      digitalWrite(Lm2, LOW);      digitalWrite(Rm2, LOW);
     }
 
-    else if (val == 'B')
-    {
-      digitalWrite(lm2, HIGH);   digitalWrite(rm2, HIGH);
-      digitalWrite(lm1, LOW);  digitalWrite(rm1, LOW);
-
+    else if (val == 'B'){
+      digitalWrite(Lm2, HIGH);   digitalWrite(Rm2, HIGH);
+      digitalWrite(Lm1, LOW);  digitalWrite(Rm1, LOW);
     }
-    else if (val == 'R')
-    {
-      digitalWrite(lm1, HIGH);  digitalWrite(rm2, HIGH);
-      digitalWrite(lm2, LOW);       digitalWrite(rm1, LOW);
+    
+    else if (val == 'R'){
+      digitalWrite(Lm1, HIGH);  digitalWrite(Rm2, HIGH);
+      digitalWrite(Lm2, LOW);       digitalWrite(Rm1, LOW);
     }
-   
-    else if (val == 'L')
-    {
-      digitalWrite(lm2, HIGH);  digitalWrite(rm1, HIGH);
-      digitalWrite(lm1, LOW);       digitalWrite(rm2, LOW);
+    
+    else if (val == 'L'){
+      digitalWrite(Lm2, HIGH);  digitalWrite(Rm1, HIGH);
+      digitalWrite(Lm1, LOW);       digitalWrite(Rm2, LOW);
     }
 
     else  {
-      digitalWrite(lm1, LOW);      digitalWrite(rm1, LOW);
-      digitalWrite(lm2, LOW);       digitalWrite(rm2, LOW);
-
+      digitalWrite(Lm1, LOW);      digitalWrite(Rm1, LOW);
+      digitalWrite(Lm2, LOW);       digitalWrite(Rm2, LOW);
     }
 
- }
+  }
 }
